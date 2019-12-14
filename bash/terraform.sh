339aabb0111d17df093a14739e9f68f2c9a02480
Norman Torres <norman.torres.mx@gmail.com>
2019-12-14 03:45:49 -0600

    jenkins

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -1,1 +1,6 @@
+wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip
+unzip /tmp/terraform.zip -d ~/bin
 export TF_VAR_image_id=$(curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"tec-prod-norman404/app:17\") | .id")
+echo $TF_VAR_image_id
+cd terraform && ./tmp/terraform apply && cd ..
+
