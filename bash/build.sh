abc4adc3764472ad3d7ea2ac74103f07197a7411
Norman Torres <norman.torres.mx@gmail.com>
2019-12-14 04:46:04 -0600

    correr

--- bash/build.sh
+++ bash/build.sh
@@ -1,5 +1,5 @@
 # wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip
 # unzip /tmp/packer.zip
-# packer validate deployments/template.json
-# packer build deployments/template.json
-echo "Ok"
+packer validate deployments/template.json
+packer build deployments/template.json
+# echo "Ok"
