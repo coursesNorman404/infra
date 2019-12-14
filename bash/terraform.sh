dfdc3b1b62a98ab20526f68a2990961e9093a7e9
Norman Torres <norman.torres.mx@gmail.com>
2019-12-14 04:26:23 -0600

    init

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -3,6 +3,7 @@
 export TF_VAR_image_id=$(curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"tec-prod-norman404/app:17\") | .id")
 echo $TF_VAR_image_id
 cd terraform
+terraform init
 terraform apply
 cd ..
 
